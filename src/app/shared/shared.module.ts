7e7bc488e25d2222e89e57fee70b0bc70063dacd
Said Roohullah Allem <s.r.alem19@gmail.com>
2022-10-24 23:43:21 +0200

    created carousel

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagerComponent } from './components/pager/pager.component';
-
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @NgModule({
   declarations: [
@@ -12,8 +12,9 @@ import { PagerComponent } from './components/pager/pager.component';
   ],
   imports: [
     CommonModule,
-    PaginationModule.forRoot() // as the pagination module has its own provider's array and those providers need to be injected into our routes module and start-up. So this is effectively acting as a singleton anyway. And if we take off the forRoot(), then it won't load with its providers and will have errors.
+    PaginationModule.forRoot(), // as the pagination module has its own provider's array and those providers need to be injected into our routes module and start-up. So this is effectively acting as a singleton anyway. And if we take off the forRoot(), then it won't load with its providers and will have errors.
+    CarouselModule.forRoot(),
   ],
-  exports: [PaginationModule, PagingHeaderComponent, PagerComponent]
+  exports: [PaginationModule, PagingHeaderComponent, PagerComponent, CarouselModule]
 })
 export class SharedModule { }
