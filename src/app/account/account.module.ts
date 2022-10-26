93297ac585a578b1adb11b1b93781c4ae6afd7c1
Said Roohullah Allem <s.r.alem19@gmail.com>
2022-10-26 14:28:02 +0200

    created account module

---
+++ src/app/account/account.module.ts
@@ -0,0 +1,18 @@
+import { AccountRoutingModule } from './account-routing.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    CommonModule,
+    AccountRoutingModule
+  ],
+
+})
+export class AccountModule { }
