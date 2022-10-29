8e429f23629e839433ee2b9a4e5b0dd343282986
Said Roohullah Allem <s.r.alem19@gmail.com>
2022-10-29 02:29:20 +0200

    init checkout address, delivery, review, payment and success components

--- src/app/checkout/checkout.module.ts
+++ src/app/checkout/checkout.module.ts
@@ -3,12 +3,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckoutComponent } from './checkout.component';
 import { SharedModule } from '../shared/shared.module';
+import { CheckoutAddressComponent } from './checkout-address/checkout-address.component';
+import { CheckoutDeliveryComponent } from './checkout-delivery/checkout-delivery.component';
+import { CheckoutReviewComponent } from './checkout-review/checkout-review.component';
+import { CheckoutPaymentComponent } from './checkout-payment/checkout-payment.component';
+import { CheckoutSuccessComponent } from './checkout-success/checkout-success.component';
 
 
 
 @NgModule({
   declarations: [
-    CheckoutComponent
+    CheckoutComponent,
+    CheckoutAddressComponent,
+    CheckoutDeliveryComponent,
+    CheckoutReviewComponent,
+    CheckoutPaymentComponent,
+    CheckoutSuccessComponent
   ],
   imports: [
     CommonModule,
