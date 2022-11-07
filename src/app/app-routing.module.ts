c6eb92232bcef8e80ac0b8467e80f5ed3d695a87
Said Roohullah Allem <s.r.alem19@gmail.com>
2022-11-07 12:01:27 +0100

    created about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ const routes: Routes = [
   { path: 'checkout', canActivate: [AuthGuard], loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule), data: { breadcrumb: 'Checkout' } },
   { path: 'account', loadChildren: () => import('./account/account.module').then(mod => mod.AccountModule), data: { breadcrumb: { skip: true } } },
   { path: 'orders', canActivate: [AuthGuard], loadChildren: () => import('./orders/orders.module').then(mod => mod.OrdersModule), data: { breadcrumb: 'orders' } },
+  { path: 'about', loadChildren: () => import('./about/about.module').then(mod => mod.AboutModule), data: { breadcrumb: 'About' } },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full' },
 ];
 
