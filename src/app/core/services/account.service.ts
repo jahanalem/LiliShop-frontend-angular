b4b9be91a899d8e129b7c5cf245aae89e59cd213
Said Roohullah Allem <s.r.alem19@gmail.com>
2023-04-10 15:20:00 +0200

    refactor login.component

--- src/app/core/services/account.service.ts
+++ src/app/core/services/account.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { map, Observable, of, ReplaySubject } from 'rxjs';
+import { of, Observable, ReplaySubject, tap } from 'rxjs';
 import { IAddress } from 'src/app/shared/models/address';
 import { IUser } from 'src/app/shared/models/user';
 import { environment } from 'src/environments/environment';
@@ -10,22 +10,22 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class AccountService {
-          baseUrl           = environment.apiUrl;
+  private readonly baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<IUser | null>(1);
-          currentUser$      = this.currentUserSource.asObservable();
+  public currentUser$ = this.currentUserSource.asObservable();
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
-  loadCurrentUser(token: string | null): Observable<void> {
+  loadCurrentUser(token: string | null): Observable<IUser | null> {
     if (token === null) {
       this.currentUserSource.next(null);
-      return of();
+      return of(null);
     }
     let headers = new HttpHeaders();
-        headers = headers.set('Authorization', `Bearer ${token}`);
+    headers = headers.set('Authorization', `Bearer ${token}`);
 
-    return this.http.get<IUser>(this.baseUrl + 'account/currentuser', {headers: headers }).pipe(
-      map((user: IUser) => {
+    return this.http.get<IUser>(`${this.baseUrl}account/currentuser`, { headers }).pipe(
+      tap((user: IUser) => {
         if (user) {
           localStorage.setItem('token', user.token);
           this.currentUserSource.next(user);
@@ -34,9 +34,9 @@ export class AccountService {
     );
   }
 
-  login(values: any): Observable<void> {
-    return this.http.post<IUser>(this.baseUrl + 'account/login', values).pipe(
-      map((user: IUser) => {
+  login(values: any): Observable<IUser> {
+    return this.http.post<IUser>(`${this.baseUrl}account/login`, values).pipe(
+      tap((user: IUser) => {
         if (user) {
           localStorage.setItem('token', user.token);
           this.currentUserSource.next(user);
@@ -45,9 +45,9 @@ export class AccountService {
     );
   }
 
-  register(values: any): Observable<void> {
-    return this.http.post<IUser>(this.baseUrl + 'account/register', values).pipe(
-      map((user: IUser) => {
+  register(values: any): Observable<IUser> {
+    return this.http.post<IUser>(`${this.baseUrl}account/register`, values).pipe(
+      tap((user: IUser) => {
         if (user) {
           localStorage.setItem('token', user.token);
           this.currentUserSource.next(user);
@@ -63,14 +63,14 @@ export class AccountService {
   }
 
   checkEmailExists(email: string): Observable<boolean> {
-    return this.http.get<boolean>(this.baseUrl + 'account/emailexists?email=' + email);
+    return this.http.get<boolean>(`${this.baseUrl}account/emailexists?email=${email}`);
   }
 
   getUserAddress(): Observable<IAddress> {
-    return this.http.get<IAddress>(this.baseUrl + 'account/address');
+    return this.http.get<IAddress>(`${this.baseUrl}account/address`);
   }
 
   updateAddress(address: IAddress): Observable<IAddress> {
-    return this.http.put<IAddress>(this.baseUrl + 'account/address', address);
+    return this.http.put<IAddress>(`${this.baseUrl}account/address`, address);
   }
 }
--- src/app/features/user-area/account/login/login.component.ts
+++ src/app/features/user-area/account/login/login.component.ts
@@ -2,22 +2,25 @@ import { PERMISSION_KIND, PERMISSION_NAME } from 'src/app/shared/constants/auth'
 import { IUser } from 'src/app/shared/models/user';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { pattern } from 'src/app/shared/constants/patterns';
 import { AccountService } from 'src/app/core/services/account.service';
 
-  /* sample form: https://mdbootstrap.com/docs/standard/extended/login/ */
+
+/* sample form: https://mdbootstrap.com/docs/standard/extended/login/ */
 @Component({
-  selector   : 'app-login',
+  selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls  : ['./login.component.scss']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
-  returnUrl : string = '/';
+  returnUrl: string = '/';
 
   constructor(private accountService: AccountService,
-    private router: Router, private activatedRoute: ActivatedRoute) { }
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'] || '/shop';
@@ -25,24 +28,28 @@ export class LoginComponent implements OnInit {
   }
 
   createLoginForm() {
-    this.loginForm = new FormGroup({
-      email   : new FormControl('', [Validators.required, Validators.pattern(pattern.EMAIL)]),
-      password: new FormControl('', Validators.required),
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.pattern(pattern.EMAIL)]],
+      password: ['', Validators.required],
     });
   }
 
+
   onSubmit() {
-    this.accountService.login(this.loginForm.value).subscribe(() => {
-      this.accountService.currentUser$.subscribe((user: IUser | null) => {
-        if (user) {
-          if (PERMISSION_KIND[PERMISSION_NAME.PRIVATE_ACCESS].includes(user.role)) {
-            this.router.navigateByUrl('admin');
+    this.accountService.login(this.loginForm.value)
+      .subscribe({
+        next: (user: IUser | null) => {
+          if (user) {
+            if (PERMISSION_KIND[PERMISSION_NAME.PRIVATE_ACCESS].includes(user.role)) {
+              this.router.navigateByUrl('admin');
+            } else {
+              this.router.navigateByUrl(this.returnUrl);
+            }
           }
-          else {
-            this.router.navigateByUrl(this.returnUrl);
-          }
+        },
+        error: () => {
+          console.log("Submit failed!");
         }
       });
-    }, () => { console.log("Submit failed!"); })
   }
 }
