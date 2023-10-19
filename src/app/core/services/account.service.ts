21f15bc2bc8720754a2a448383a717fd52da6005
Said Roohullah Allem <s.r.alem19@gmail.com>
2023-10-19 14:58:28 +0200

    ref: Refactor and document AccountService methods

--- src/app/core/services/account.service.ts
+++ src/app/core/services/account.service.ts
@@ -18,16 +18,30 @@ export class AccountService {
 
   constructor(private http: HttpClient, private router: Router, private storageService: StorageService) { }
 
+  /**
+  * Loads the current user based on the provided token and updates the user state.
+  *
+  * If the token is null, the current user state is set to null and an Observable
+  * of null is returned. Otherwise, it sends a GET request to the server to fetch
+  * the current user and updates the user state.
+  *
+  * @param {string | null} token - The authentication token.
+  * @returns {Observable<IUser | null>} An Observable of the user object or null.
+  */
   loadCurrentUser(token: string | null): Observable<IUser | null> {
-    if (token === null) {
+    // If token is null, reset the current user state and return null.
+    if (!token) {
       this.currentUserSource.next(null);
       return of(null);
     }
-    let headers = new HttpHeaders();
-    headers = headers.set('Authorization', `Bearer ${token}`);
 
+    // Set up the Authorization header with the token.
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    // Make the HTTP request to fetch the current user.
     return this.http.get<IUser>(`${this.baseUrl}account/currentuser`, { headers }).pipe(
       tap((user: IUser) => {
+        // If a user is returned, update the user state.
         if (user) {
           this.storageService.set(LOCAL_STORAGE_KEYS.AUTH_TOKEN, user.token);
           this.currentUserSource.next(user);
@@ -36,43 +50,110 @@ export class AccountService {
     );
   }
 
+  /**
+ * Authenticates a user by sending their login credentials to the server.
+ *
+ * Sends a POST request to the server with the user's login credentials.
+ * If authentication is successful, the method updates the current user state
+ * and stores the user's token in local storage.
+ *
+ * @param {any} values - The user's login credentials.
+ * @returns {Observable<IUser>} An Observable of the authenticated user object.
+ */
   login(values: any): Observable<IUser> {
     return this.http.post<IUser>(`${this.baseUrl}account/login`, values).pipe(
       tap((user: IUser) => {
         if (user) {
-          this.storageService.set(LOCAL_STORAGE_KEYS.AUTH_TOKEN, user.token);
-          this.currentUserSource.next(user);
+          this.updateCurrentUserState(user);
         }
       })
     );
   }
 
+  /**
+   * Registers a new user by sending their registration details to the server.
+   *
+   * Sends a POST request to the server with the user's registration details.
+   * If registration is successful, updates the current user state and stores
+   * the user's token in local storage.
+   *
+   * @param {any} values - The user's registration details.
+   * @returns {Observable<IUser>} An Observable of the newly registered user object.
+   */
   register(values: any): Observable<IUser> {
     return this.http.post<IUser>(`${this.baseUrl}account/register`, values).pipe(
       tap((user: IUser) => {
         if (user) {
-          this.storageService.set(LOCAL_STORAGE_KEYS.AUTH_TOKEN, user.token);
-          this.currentUserSource.next(user);
+          this.updateCurrentUserState(user);
         }
       })
     );
   }
 
+  /**
+   * Logs out the current user.
+   *
+   * Deletes the user's authentication token from local storage, resets the
+   * current user state to null, and navigates to the home page.
+   */
   logout(): void {
     this.storageService.delete(LOCAL_STORAGE_KEYS.AUTH_TOKEN);
-    this.currentUserSource.next(null);
+    this.resetCurrentUserState();
     this.router.navigateByUrl('/');
   }
 
+  /**
+ * Checks if the given email already exists in the system.
+ *
+ * Sends a GET request to the server to check if the email exists.
+ *
+ * @param {string} email - The email address to check.
+ * @returns {Observable<boolean>} An Observable indicating whether the email exists.
+ */
   checkEmailExists(email: string): Observable<boolean> {
-    return this.http.get<boolean>(`${this.baseUrl}account/emailexists?email=${email}`);
+    const url = `${this.baseUrl}account/emailexists?email=${email}`;
+    return this.http.get<boolean>(url);
   }
 
+  /**
+   * Fetches the user's address from the server.
+   *
+   * Sends a GET request to the server to retrieve the user's address.
+   *
+   * @returns {Observable<IAddress>} An Observable of the user's address.
+   */
   getUserAddress(): Observable<IAddress> {
-    return this.http.get<IAddress>(`${this.baseUrl}account/address`);
+    const url = `${this.baseUrl}account/address`;
+    return this.http.get<IAddress>(url);
   }
 
+  /**
+   * Updates the user's address in the system.
+   *
+   * Sends a PUT request to the server to update the user's address.
+   *
+   * @param {IAddress} address - The new address details.
+   * @returns {Observable<IAddress>} An Observable of the updated address.
+   */
   updateAddress(address: IAddress): Observable<IAddress> {
-    return this.http.put<IAddress>(`${this.baseUrl}account/address`, address);
+    const url = `${this.baseUrl}account/address`;
+    return this.http.put<IAddress>(url, address);
+  }
+
+  /**
+   * Updates the current user state and stores the user's token in local storage.
+   *
+   * @param {IUser} user - The authenticated user object.
+   */
+  private updateCurrentUserState(user: IUser): void {
+    this.storageService.set(LOCAL_STORAGE_KEYS.AUTH_TOKEN, user.token);
+    this.currentUserSource.next(user);
+  }
+
+  /**
+   * Resets the current user state to null.
+   */
+  private resetCurrentUserState(): void {
+    this.currentUserSource.next(null);
   }
 }
