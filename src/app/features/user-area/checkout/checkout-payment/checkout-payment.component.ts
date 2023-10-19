4283e3d026e45757fd95b7dea15e7f9b2237d637
Said Roohullah Allem <s.r.alem19@gmail.com>
2023-10-20 00:56:30 +0200

    ref: Optimize and document  CheckoutPaymentComponent methods

--- src/app/features/user-area/checkout/checkout-payment/checkout-payment.component.ts
+++ src/app/features/user-area/checkout/checkout-payment/checkout-payment.component.ts
@@ -79,71 +79,130 @@ export class CheckoutPaymentComponent implements AfterViewInit, OnDestroy {
     this.cardCvc.on('change', this.cardHandler);
   }
 
+  /**
+ * Asynchronously submits the order.
+ * This method handles both the order creation and the Stripe payment confirmation.
+ */
   async submitOrder() {
+    // Start the loading spinner.
     this.loading = true;
 
+    // Retrieve the current basket value.
     const basket = this.basketService.getCurrentBasketValue();
+
+    // If the basket is null or undefined, stop the loading spinner and return.
     if (!basket) {
       this.loading = false;
       return;
     }
 
     try {
+      // Perform both the order creation and Stripe payment confirmation in parallel.
       const [createdOrder, paymentResult] = await Promise.all([
         this.createOrder(basket),
         this.confirmPaymentWithStripe(basket),
       ]);
 
+      // If the payment is successful, proceed with the order.
       if (paymentResult.paymentIntent) {
-        this.basketService.deleteBasket(basket).subscribe({
-          next: () => console.log('Basket deleted'),
-          error: (error: any) => { console.error('Error deleting basket:', error); },
-        });
-        const navigationExtras: NavigationExtras = { state: { order: createdOrder } };
-        this.router.navigate(['checkout/success'], navigationExtras);
+        this.handleSuccessfulPayment(createdOrder, basket);
       } else {
-        // It means that card has failed to be accepted by Stripe for whatever reason.
+        // Handle card failure or other Stripe issues.
         this.toastr.error(paymentResult.error.message);
       }
     } catch (error) {
-      console.log(error);
+      console.error('An error occurred while submitting the order:', error);
     } finally {
+      // Stop the loading spinner.
       this.loading = false;
     }
   }
 
+  /**
+   * Handles successful payments by deleting the basket and navigating to the success page.
+   * @param createdOrder - The created order object.
+   * @param basket - The current basket object.
+   */
+  private handleSuccessfulPayment(createdOrder: IOrder, basket: IBasket) {
+    this.basketService.deleteBasket(basket).subscribe({
+      next: () => console.log('Basket deleted'),
+      error: (error: any) => console.error('Error deleting basket:', error),
+    });
+
+    // Pass the created order to the success page as state data.
+    const navigationExtras: NavigationExtras = { state: { order: createdOrder } };
+    this.router.navigate(['checkout/success'], navigationExtras);
+  }
+
+
+  /**
+  * Confirm payment with Stripe.
+  * This method sends the payment information to Stripe for payment processing.
+  *
+  * @param basket - The basket object containing the client secret for Stripe.
+  * @returns A promise that resolves with the Stripe PaymentIntentResult.
+  * @throws Will throw an error if the client secret is missing.
+  */
   public async confirmPaymentWithStripe(basket: IBasket): Promise<PaymentIntentResult> {
+
+    // Validate that the client secret exists
     if (!basket.clientSecret) {
       const errorMsg = 'Client secret is missing';
-      this.toastr.error(errorMsg);
-      throw new Error(errorMsg);
+      this.toastr.error(errorMsg);  // Show error notification
+      throw new Error(errorMsg);  // Throw an error
     }
-    return this.stripe.confirmCardPayment(basket.clientSecret, {
-      payment_method: {
-        card: this.cardNumber,
-        billing_details: {
-          name: this.checkoutForm.get('paymentForm')?.get('nameOnCard')?.value,
-        },
+
+    // Define payment method details
+    const paymentMethodDetails = {
+      card: this.cardNumber,
+      billing_details: {
+        name: this.checkoutForm.get('paymentForm')?.get('nameOnCard')?.value,
       },
+    };
+
+    // Confirm card payment with Stripe
+    return this.stripe.confirmCardPayment(basket.clientSecret, {
+      payment_method: paymentMethodDetails,
     });
   }
 
+  /**
+   * Create a new order.
+   * This method sends the order information to the backend service for processing.
+   *
+   * @param basket - The basket object to be converted into an order.
+   * @returns A promise that resolves with the created order object.
+   * @throws Will throw an error if the order creation fails.
+   */
   public async createOrder(basket: IBasket): Promise<IOrder> {
     try {
+      // Create an order object based on the current basket
       const orderToCreate = this.getOrderToCreate(basket);
+
+      // Send the order to the backend and wait for a response
       return await lastValueFrom(this.checkoutService.createOrder(orderToCreate));
     } catch (error) {
-      console.error('Error creating order:', error);
-      throw error;
+      console.error('Error creating order:', error); // Log the error for debugging
+      throw error; // Re-throw the error to be handled by the caller
     }
   }
 
+  /**
+   * Constructs an object to represent the order that will be created.
+   *
+   * @param basket - The current basket object containing items to be ordered.
+   * @returns An object conforming to the IOrderToCreate interface.
+   */
   private getOrderToCreate(basket: IBasket): IOrderToCreate {
+    // Retrieve the delivery and address forms from the checkout form group
     const deliveryForm = this.checkoutForm.get('deliveryForm');
     const addressForm = this.checkoutForm.get('addressForm');
+
+    // Extract the selected delivery method ID and shipping address
     const deliveryMethodId = deliveryForm?.get('deliveryMethod')?.value;
     const shipToAddress = addressForm?.value;
 
+    // Construct and return the order object
     return {
       basketId: basket.id,
       deliveryMethodId,
