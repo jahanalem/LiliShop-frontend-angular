f2a95d37c0a122a3a68917245f8f693db4d952c3
jahanalem <s.r.alem19@gmail.com>
2025-05-11 23:53:35 +0200

    feat(discount-form): implement basic structure with stepper, tiers, and condition groups

--- src/app/features/admin-area/admin/discounts/edit-discount/edit-discount.component.ts
+++ src/app/features/admin-area/admin/discounts/edit-discount/edit-discount.component.ts
@@ -1,11 +1,159 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+// Angular Material Modules
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { DiscountTargetType, IDiscount } from 'src/app/shared/models/discount-system';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatDivider } from '@angular/material/divider';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-edit-discount',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+
+    // Material Modules
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
+    MatExpansionModule,
+    MatTooltipModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatDivider,
+    MatCheckboxModule
+  ],
   templateUrl: './edit-discount.component.html',
   styleUrl: './edit-discount.component.scss'
 })
 export class EditDiscountComponent {
+  isEditMode = false;
 
+  discountInfoForm!: FormGroup;
+  tiersForm!: FormGroup;
+  discountGroupForm!: FormGroup;
+
+  targetEntityOptions = [
+    { label: 'Product Type', value: DiscountTargetType.ProductType },
+    { label: 'Product Brand', value: DiscountTargetType.ProductBrand },
+    { label: 'Size', value: DiscountTargetType.Size },
+    { label: 'Product', value: DiscountTargetType.Product },
+    { label: 'All', value: DiscountTargetType.All }
+  ];
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.initForms();
+  }
+
+  private initForms(): void {
+    this.discountInfoForm = this.fb.group({
+      name: ['', Validators.required],
+      startDate: ['', Validators.required],
+      endDate: ['', Validators.required],
+      isActive: [false]
+    });
+
+    this.tiersForm = this.fb.group({
+      tiers: this.fb.array([])
+    });
+
+    this.discountGroupForm = this.fb.group({
+      conditionGroups: this.fb.array([])
+    });
+  }
+
+  get tiers(): FormArray {
+    return this.tiersForm.get('tiers') as FormArray;
+  }
+
+  addTier(): void {
+    this.tiers.push(this.fb.group({
+      amount: [0, Validators.required],
+      isPercentage: [false],
+      isFreeShipping: [false]
+    }));
+  }
+
+  removeTier(i: number): void {
+    this.tiers.removeAt(i);
+  }
+
+  get conditionGroups(): FormArray {
+    return this.discountGroupForm.get('conditionGroups') as FormArray;
+  }
+
+  addConditionGroup(): void {
+    this.conditionGroups.push(this.fb.group({
+      tierIndex: [0, Validators.required],
+      conditions: this.fb.array([])
+    }));
+  }
+
+  removeConditionGroup(i: number): void {
+    this.conditionGroups.removeAt(i);
+  }
+
+  getConditions(groupIndex: number): FormArray {
+    return this.conditionGroups.at(groupIndex).get('conditions') as FormArray;
+  }
+
+  addCondition(groupIndex: number): void {
+    this.getConditions(groupIndex).push(this.fb.group({
+      targetEntity: [DiscountTargetType.Product, Validators.required],
+      targetEntityId: [null, Validators.required],
+      shouldNotify: [false]
+    }));
+  }
+
+  removeCondition(groupIndex: number, conditionIndex: number): void {
+    this.getConditions(groupIndex).removeAt(conditionIndex);
+  }
+
+  submit(): void {
+    if (this.discountInfoForm.invalid || this.tiersForm.invalid || this.discountGroupForm.invalid) {
+      console.warn('Form invalid');
+      return;
+    }
+
+    const discount: IDiscount = {
+      ...this.discountInfoForm.value,
+      tiers: this.tiersForm.value.tiers,
+      discountGroup: {
+        name: this.discountInfoForm.value.name + '_group', // Or some logic
+        conditionGroups: this.discountGroupForm.value.conditionGroups
+      }
+    };
+
+    if (this.isEditMode) {
+      console.log('Update Discount', discount);
+      // Call update service
+    } else {
+      console.log('Create Discount', discount);
+      // Call create service
+    }
+  }
 }
